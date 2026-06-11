d8d088521e391155d76ee440d69f67e7baf03d14
Yuriy <yuriy@example.com>
2026-06-11 10:40:31 +0100

    Initial commit

---
+++ lessons/lesson 1/main.py
@@ -0,0 +1,44 @@
+from operator import truediv
+#1
+str_animals = 'dog', 'cat', 'mouse'
+str_capital = 'London'
+num1 = 365
+num2 = 500
+tea = True
+juice = False
+print(str_animals, str_capital, num1, num2, tea, juice,sep='\n')
+
+#2
+red = 50
+black = 32
+print(red + black)
+print(red - black)
+print(red * black)
+print(red / black)
+print(red % black)
+
+#3
+first_name = 'Yurii'
+last_name = 'Sorokin'
+full_name = (first_name +" " + last_name)
+print(full_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
