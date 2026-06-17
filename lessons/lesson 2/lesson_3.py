5be0f8c58bbe2cf9f096e304b29dd1a81eaa1e5e
Yuriy <yuriy@example.com>
2026-06-17 15:27:01 +0100

    добавил урок 3

---
+++ lessons/lesson 2/lesson_3.py
@@ -0,0 +1,23 @@
+fruit_stock = {'apple':10, 'banana':15, 'orange':8}
+print(fruit_stock)
+print(fruit_stock['apple'])
+fruit_stock['pear']=12
+print(fruit_stock['pear'])
+fruit_stock['banana']=20
+print(fruit_stock)
+
+shopping_list = ['молоко', 'хлеб', 'яйца', 'сыр']
+print(shopping_list)
+print(shopping_list[0])
+
+dogs = [
+    {"name": "Рекс", "age": 5, "breed": "Лабрадор"},
+    {"name": "Бім", "age": 3, "breed": "Хаскі"},
+    {"name": "Тузик", "age": 4, "breed": "Вівчарка"},
+    {"name": "Барні", "age": 2, "breed": "Мопс"},
+    {"name": "Чарлі", "age": 6, "breed": "Бульдог"},
+    {"name": "Лакі", "age": 1, "breed": "Коргі"},
+    {"name": "Макс", "age": 7, "breed": "Доберман"}
+]
+print(dogs[2]["name"])
+
